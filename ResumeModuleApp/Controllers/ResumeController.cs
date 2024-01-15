e99a379d6c261b09bb53ff2fa1fc1765f2dc7544
ErzaBlakajj <eb57603@ubt-uni.net>
2024-01-15 15:06:13 +0100

    commit

--- ResumeModuleApp/Controllers/ResumeController.cs
+++ ResumeModuleApp/Controllers/ResumeController.cs
@@ -44,5 +44,47 @@ namespace ResumeModuleApp.Controllers
 
             return Ok(resume);
         }
+        [HttpDelete("delete/{id}")]
+        public async Task<IActionResult> DeleteResume(int id)
+        {
+            var resume = _context.Resumes.Find(id);
+
+            if (resume == null)
+            {
+                return NotFound();
+            }
+
+            _context.Resumes.Remove(resume);
+            await _context.SaveChangesAsync();
+
+            return NoContent();
+        }
+        [HttpPut("update/{id}")]
+        public async Task<IActionResult> UpdateResume(int id, [FromBody] Resume updatedResume)
+        {
+            if (updatedResume == null || id != updatedResume.ResumeId)
+            {
+                return BadRequest("Invalid data or mismatched id");
+            }
+
+            var existingResume = _context.Resumes.Find(id);
+
+            if (existingResume == null)
+            {
+                return NotFound();
+            }
+
+            existingResume.ResumeId = updatedResume.ResumeId;
+            existingResume.ApplicantName = updatedResume.ApplicantName;
+            existingResume.Education = updatedResume.Education;
+            existingResume.Experience = updatedResume.Experience;
+            existingResume.Position = updatedResume.Position;
+            existingResume.skills = updatedResume.skills;
+
+            _context.Resumes.Update(existingResume);
+            await _context.SaveChangesAsync();
+
+            return Ok(existingResume);
+        }
     }
 }
