30028f26564c1671196ad237da9897c144413d8d
ErzaBlakajj <eb57603@ubt-uni.net>
2024-01-18 14:52:45 +0100

    commit

---
+++ ResumeModuleApp/Controllers/Skills.cs
@@ -0,0 +1,94 @@
+﻿
+
+
+
+
+
+
+using Microsoft.AspNetCore.Mvc;
+using ResumeModuleApp.DataService;
+using ResumeModuleApp.Models;
+
+namespace ResumeModuleApp.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class SkillsController : Controller
+    {
+
+        private ResumeContext _context;
+
+        public SkillsController(ResumeContext context)
+        {
+            _context = context;
+        }
+
+        [HttpPost("create")]
+        public async Task<IActionResult> AddResume([FromBody] Skills skills)
+        {
+            if (skills == null)
+            {
+                return BadRequest("Invalid resume data");
+            }
+
+            _context.Skills.Add(skills);
+            await _context.SaveChangesAsync();
+
+            return CreatedAtAction(nameof(GetSkills), new { id =skills.SkillsId }, skills);
+        }
+
+        [HttpGet("{id}")]
+        public IActionResult GetSkills(int id)
+        {
+            var skills = _context.Skills.Find(id);
+
+
+
+            if (skills == null)
+            {
+                return NotFound();
+            }
+
+            return Ok(skills);
+        }
+        [HttpDelete("delete/{id}")]
+        public async Task<IActionResult> DeleteSkills(int id)
+        {
+            var skills = _context.Skills.Find(id);
+
+            if (skills == null)
+            {
+                return NotFound();
+            }
+
+            _context.Skills.Remove(skills);
+            await _context.SaveChangesAsync();
+
+            return NoContent();
+        }
+        [HttpPut("update/{id}")]
+        public async Task<IActionResult> UpdateSkills(int id, [FromBody] Skills updatedSkills)
+        {
+            if (updatedSkills== null || id != updatedSkills.SkillsId)
+            {
+                return BadRequest("Invalid data or mismatched id");
+            }
+
+            var existingSkills = _context.Skills.Find(id);
+
+            if (existingSkills == null)
+            {
+                return NotFound();
+            }
+
+          
+          
+            existingSkills.Description = updatedSkills.Description;
+
+            _context.Skills.Update(existingSkills);
+            await _context.SaveChangesAsync();
+
+            return Ok(existingSkills);
+        }
+    }
+}
