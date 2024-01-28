f8cbd60fe4e7ac1cdc0e4449d7e3637e90695ccf
ErzaBlakajj <eb57603@ubt-uni.net>
2024-01-28 20:47:33 +0100

    commit

--- ResumeModuleApp/DataService/ResumePDFfile.cs
+++ ResumeModuleApp/DataService/ResumePDFfile.cs
@@ -1,4 +1,5 @@
-﻿using iText.Kernel.Pdf;
+﻿using iText.Kernel.Colors;
+using iText.Kernel.Pdf;
 using iText.Layout;
 using iText.Layout.Borders;
 using iText.Layout.Element;
@@ -8,10 +9,119 @@ using System;
 using System.IO;
 using Microsoft.EntityFrameworkCore;
 using ResumeModuleApp.Models;
-using iText.Kernel.Colors;
 
 namespace ResumeModuleApp.DataService
 {
+    public interface IResumePdfBuilder
+    {
+        void BuildUserInformationSection(User user);
+        void BuildResumeInformationSection(Resume resume);
+        void BuildSkillsAndExperiencesSection(Resume resume);
+    }
+
+    public class ResumePdfBuilder : IResumePdfBuilder
+    {
+        private Document _document;
+
+        public ResumePdfBuilder(Document document)
+        {
+            _document = document;
+        }
+
+        public void BuildUserInformationSection(User user)
+        {
+            _document.Add(new Paragraph("\n"));
+
+            var titleStyle = new Style()
+                .SetFontSize(14)
+                .SetBold();
+
+            var titleParagraph = new Paragraph("User Information")
+                .AddStyle(titleStyle)
+                .SetBackgroundColor(DeviceRgb.RED);
+
+            _document.Add(titleParagraph);
+
+            var userTable = new Table(2).UseAllAvailableWidth();
+            userTable.SetBorder(new SolidBorder(1f));
+            userTable.AddCell(CreateCell("First Name:").AddStyle(titleStyle));
+            userTable.AddCell(CreateCell(user.Emri));
+            userTable.AddCell(CreateCell("Last Name:").AddStyle(titleStyle));
+            userTable.AddCell(CreateCell(user.Mbiemri));
+            userTable.AddCell(CreateCell("Email:").AddStyle(titleStyle));
+            userTable.AddCell(CreateCell(user.Email));
+
+            _document.Add(userTable);
+        }
+
+        public void BuildResumeInformationSection(Resume resume)
+        {
+            _document.Add(new Paragraph("\n"));
+
+            var titleStyle = new Style()
+                .SetFontSize(14)
+                .SetBold();
+
+            var titleParagraph = new Paragraph("Resume Information")
+                .AddStyle(titleStyle)
+                .SetBackgroundColor(DeviceRgb.RED);
+
+            _document.Add(titleParagraph);
+
+            var infoTable = new Table(2).UseAllAvailableWidth();
+            infoTable.SetBorder(new SolidBorder(1f));
+            infoTable.AddCell(CreateCell("Education:").AddStyle(titleStyle));
+            infoTable.AddCell(CreateCell(resume.Education));
+            infoTable.AddCell(CreateCell("Position:").AddStyle(titleStyle));
+            infoTable.AddCell(CreateCell(resume.Position));
+            infoTable.AddCell(CreateCell("Languages:").AddStyle(titleStyle));
+            infoTable.AddCell(CreateCell(resume.Languages));
+
+            _document.Add(infoTable);
+        }
+
+        public void BuildSkillsAndExperiencesSection(Resume resume)
+        {
+            _document.Add(new Paragraph("\n"));
+
+            var titleStyle = new Style()
+                .SetFontSize(14)
+                .SetBold();
+
+            var titleParagraph = new Paragraph("Skills and Experiences")
+                .AddStyle(titleStyle)
+                .SetBackgroundColor(DeviceRgb.RED);
+
+            _document.Add(titleParagraph);
+
+            var skillsAndExperiencesTable = new Table(2).UseAllAvailableWidth();
+            skillsAndExperiencesTable.SetBorder(new SolidBorder(1f));
+
+            var skillsCell = new Cell().SetBorder(Border.NO_BORDER);
+            skillsCell.Add(new Paragraph("Skills").AddStyle(titleStyle));
+            foreach (var skill in resume.Skills)
+            {
+                skillsCell.Add(new Paragraph($"- {skill.Description}").SetBorder(Border.NO_BORDER));
+            }
+            skillsAndExperiencesTable.AddCell(skillsCell);
+
+            var experiencesCell = new Cell().SetBorder(Border.NO_BORDER);
+            experiencesCell.Add(new Paragraph("Experiences").AddStyle(titleStyle));
+            foreach (var experience in resume.Experiences)
+            {
+                experiencesCell.Add(new Paragraph($"- {experience.Description}").SetBorder(Border.NO_BORDER));
+            }
+            skillsAndExperiencesTable.AddCell(experiencesCell);
+
+            _document.Add(skillsAndExperiencesTable);
+        }
+
+        private Cell CreateCell(string text)
+        {
+            return new Cell().Add(new Paragraph(text)).SetBorder(Border.NO_BORDER);
+        }
+    }
+
     public interface IResumePdfService
     {
         byte[] GeneratePdf(int resumeId, User user);
@@ -48,114 +158,15 @@ namespace ResumeModuleApp.DataService
 
                         document.Add(new Paragraph("Resume").SetFontSize(18).SetBold());
 
-                        AddUserInformationSection(document, user);
-
-                        AddResumeInformationSection(document, resume);
-
-                        AddSkillsAndExperiencesSection(document, resume);
+                        var builder = new ResumePdfBuilder(document);
+                        builder.BuildUserInformationSection(user);
+                        builder.BuildResumeInformationSection(resume);
+                        builder.BuildSkillsAndExperiencesSection(resume);
                     }
                 }
 
                 return stream.ToArray();
             }
-        }
-
-       
-
-        private void AddUserInformationSection(Document document, User user)
-        {
-            document.Add(new Paragraph("\n"));
-
-            var titleStyle = new Style()
-                .SetFontSize(14)
-                .SetBold();
-
-            var titleParagraph = new Paragraph("User Information")
-                .AddStyle(titleStyle)
-                .SetBackgroundColor(DeviceRgb.RED);
-
-            document.Add(titleParagraph);
-
-            var userTable = new Table(2).UseAllAvailableWidth();
-            userTable.SetBorder(new SolidBorder(1f));
-            userTable.AddCell(CreateCell("First Name:").AddStyle(titleStyle));
-            userTable.AddCell(CreateCell(user.Emri));
-            userTable.AddCell(CreateCell("Last Name:").AddStyle(titleStyle));
-            userTable.AddCell(CreateCell(user.Mbiemri));
-            userTable.AddCell(CreateCell("Email:").AddStyle(titleStyle));
-            userTable.AddCell(CreateCell(user.Email));
-
-            document.Add(userTable);
-        }
-
-        private void AddResumeInformationSection(Document document, Resume resume)
-        {
-            document.Add(new Paragraph("\n"));
-
-            var titleStyle = new Style()
-                .SetFontSize(14)
-                .SetBold();
-
-            var titleParagraph = new Paragraph("Resume Information")
-                .AddStyle(titleStyle)
-                .SetBackgroundColor(DeviceRgb.RED);
-
-            document.Add(titleParagraph);
-
-            var infoTable = new Table(2).UseAllAvailableWidth();
-            infoTable.SetBorder(new SolidBorder(1f));
-            infoTable.AddCell(CreateCell("Education:").AddStyle(titleStyle));
-            infoTable.AddCell(CreateCell(resume.Education));
-            infoTable.AddCell(CreateCell("Position:").AddStyle(titleStyle));
-            infoTable.AddCell(CreateCell(resume.Position));
-            infoTable.AddCell(CreateCell("Languages:").AddStyle(titleStyle));
-            infoTable.AddCell(CreateCell(resume.Languages));
-
-            document.Add(infoTable);
-        }
-
-        private void AddSkillsAndExperiencesSection(Document document, Resume resume)
-        {
-            document.Add(new Paragraph("\n"));
-
-            var titleStyle = new Style()
-                .SetFontSize(14)
-                .SetBold();
-
-            var titleParagraph = new Paragraph("Skills and Experiences")
-                .AddStyle(titleStyle)
-                .SetBackgroundColor(DeviceRgb.RED);
-
-            document.Add(titleParagraph);
-
-            var skillsAndExperiencesTable = new Table(2).UseAllAvailableWidth();
-            skillsAndExperiencesTable.SetBorder(new SolidBorder(1f));
-
-            var skillsCell = new Cell().SetBorder(Border.NO_BORDER);
-            skillsCell.Add(new Paragraph("Skills").AddStyle(titleStyle));
-            foreach (var skill in resume.Skills)
-            {
-                skillsCell.Add(new Paragraph($"- {skill.Description}").SetBorder(Border.NO_BORDER));
-            }
-            skillsAndExperiencesTable.AddCell(skillsCell);
-
-            var experiencesCell = new Cell().SetBorder(Border.NO_BORDER);
-            experiencesCell.Add(new Paragraph("Experiences").AddStyle(titleStyle));
-            foreach (var experience in resume.Experiences)
-            {
-                experiencesCell.Add(new Paragraph($"- {experience.Description}").SetBorder(Border.NO_BORDER));
-            }
-            skillsAndExperiencesTable.AddCell(experiencesCell);
-
-            document.Add(skillsAndExperiencesTable);
-        }
-
-        
-
-
-        private Cell CreateCell(string text)
-        {
-            return new Cell().Add(new Paragraph(text)).SetBorder(Border.NO_BORDER);
         }
     }
 }
